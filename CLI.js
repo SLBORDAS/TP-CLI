bfde9e85d26380636f8555bb58f5eab4a595accd
Matthieu Bordas <matthieu.bordas@sfr.fr>
2018-11-15 12:57:32 +0100

    code fonctionnel

--- CLI.js
+++ CLI.js
@@ -1,3 +1,26 @@
 const axios = require('axios')
+var inquirer = require('inquirer')
 
-const météo = 'http://www.meteofrance.com/previsions-meteo-france/'
+var input =  [{type: 'input',
+                    name: 'ville',
+                    message: 'choisir le nom de votre ville',}]
+
+blabla()
+
+function blabla(){
+    inquirer.prompt(input)
+        .then(answers => {
+        var cityname = answers.ville 
+        var meteo = 'http://api.openweathermap.org/data/2.5/weather?q='+cityname+'&apikey=71f44a7259695752c1d54a7c31618579'
+        axios.get (meteo)
+        .then(reponse => {
+            console.log(reponse.data.main);
+            
+        })
+        .catch(p => {
+            console.log('error',p);
+        }) 
+        console.log(meteo);
+        blabla()
+    });
+}
